1363afe4c3dcac2bd3c98574a7962b96767afab2
Ungarala Suresh <iamuvvsuresh@gmail.com>
2018-12-23 00:24:03 +0530

    updating setup

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,19 @@ var path = require('path');
 var htmlPlugin  = require('html-webpack-plugin');
 
 module.exports = {
-    entry: "./src/index.js",
+    entry: "./src/index.tsx",
     output: {
-        path: path.resolve(__dirname, dist),
+        path: path.resolve(__dirname, 'dist'),
         filename: 'app-bundle.js'
     },
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "source-map",
     module: {
         rules: [
-            { test: /\.(js)$/, use: 'babel-loader' },
-            { test: /\.css$/, use: ['style-loader,css-loader'] }
+            { test: /\.(js|jsx|ts|tsx)$/, use: 'babel-loader' },
+            { test: /\.(css|scss)$/, use: ['style-loader','css-loader','sass-loader'] },
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            { test: /\.js$/, loader: "source-map-loader", enforce: "pre" }
         ]
     },
     mode: 'development',
