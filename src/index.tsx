e5eb7452981ca0f7479d6bfb4b060f87504ad514
Ungarala Suresh <iamuvvsuresh@gmail.com>
2018-12-24 00:01:04 +0530

    still setting up...

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,12 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import './index.scss';
 
-class Cust extends React.Component{
-    render(){
-        return(
+class Cust extends React.Component {
+    render() {
+        return (
             <div id="customElem">Custom Element</div>
         );
     }
 }
 
-ReactDOM.render(<Cust/>, document.getElementById('custom'));
+ReactDOM.render(<Cust />, document.getElementById('custom'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,45 @@
-var path = require('path');
-var htmlPlugin = require('html-webpack-plugin');
+const path = require('path');
+const htmlPlugin = require('html-webpack-plugin');
 const TSLintPlugin = require('tslint-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-    entry: "./src/index.tsx",
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'app-bundle.js'
-    },
-    // Enable sourcemaps for debugging webpack's output.
-    devtool: "source-map",
-    module: {
-        rules: [
-            { test: /\.(js|jsx|ts|tsx)$/, use: 'babel-loader', exclude: /node_modules/, },
-            { test: /\.(css|scss)$/, use: ['style-loader', 'css-loader', 'sass-loader'] },
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { test: /\.(js|jsx|ts|tsx)$/, loader: "source-map-loader", enforce: "pre" },
-            { test: /\.(ts|tsx)?$/, loader: 'ts-loader', exclude: /node_modules/ }
+module.exports = (env, args) => {
+    const devMode = args.mode === 'development';
+    console.log('devMode ' + devMode);
+    return {
+        entry: "./src/index.tsx",
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'app-bundle.js'
+        },
+        // Enable sourcemaps for debugging webpack's output.
+        devtool: "source-map",
+        module: {   //for unbundled files
+            rules: [
+                { test: /\.(js|jsx|ts|tsx)$/, use: 'babel-loader', exclude: /node_modules/, },
+                // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+                { test: /\.(js|jsx|ts|tsx)$/, loader: "source-map-loader", enforce: "pre" },
+                { test: /\.(ts|tsx)?$/, loader: 'ts-loader', exclude: /node_modules/ },
+                {
+                    test: /\.(css|scss)$/, use: [
+                        devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+                        'css-loader',
+                        'sass-loader',
+                    ],
+                }
+            ]
+        },
+        plugins: [  //for bundled files
+            new htmlPlugin({
+                template: './src/index.html'
+            }),
+            new TSLintPlugin({
+                files: ['./src/**/*.ts', './src/**/*.tsx']
+            }),
+            new MiniCssExtractPlugin({  //doesn't work without 'MiniCssExtractPlugin.loader'. So, works only in prod mode.
+                filename: '[name].[hash].css',
+                chunkFilename: '[id].[hash].css',
+            })
         ]
-    },
-    mode: 'development',
-    plugins: [
-        new htmlPlugin({
-            template: './src/index.html'
-        }),
-        new TSLintPlugin({
-            files: ['./src/**/*.ts', './src/**/*.tsx']
-        })
-    ]
+    };
 }
